e90033d95271338deae4d2a183b045b72b458109
BoomBot1 <marsallmeters9694@gmail.com>
2025-06-30 01:29:09 +0700

    fixes

--- src/Exceptions/DataValidationException.php
+++ src/Exceptions/DataValidationException.php
@@ -6,6 +6,6 @@ final class DataValidationException extends BaseException
 {
     public static function failure($page): self
     {
-        return new self("Invalid data received. loc: {$page['loc']}, lastmod: {$page['lastmod']->getTimestamp()}, changefreq: {$page['changefreq']}, priority: {$page['priority']}");
+        return new self("Invalid data received. loc: {$page['loc']}, lastmod: {$page['lastmod']}, changefreq: {$page['changefreq']}, priority: {$page['priority']}");
     }
 }
